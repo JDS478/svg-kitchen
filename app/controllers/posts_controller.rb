a74f5db7906f50d54eb4daa78ec00c597ad70e37
Joel Savage <joel28169@gmail.com>
2023-03-27 17:25:35 +0100

    Added in posts controller and basic crud functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[show]
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.permit(:post).require(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
