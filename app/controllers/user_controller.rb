3d31fdc19a585b837e396ea61628bc335b9e4aa2
Joel Savage <joel28169@gmail.com>
2023-03-29 13:35:00 +0100

    Fixed form params permit bug and added avatar stylings

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,15 @@ class UserController < ApplicationController
   def show
   end
 
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
   private
+  def user_params
+    params.permit(:user).require(:first_name, :last_name, :email)
+  end
 
   def set_user
     @user = User.find(params:[id])
