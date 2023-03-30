f2313ad618ded47893262c6ff55dff99329205b1
Joel Savage <joel28169@gmail.com>
2023-03-30 13:04:28 +0100

    Added difficulties and categories for recipes, also updated seed file to compensate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_27_164946) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_112544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_164946) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_categories_on_recipe_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -58,6 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_164946) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "ingredients", default: [], array: true
+    t.bigint "category_id"
+    t.integer "difficulty"
+    t.index ["category_id"], name: "index_recipes_on_category_id"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -78,6 +89,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_27_164946) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories", "recipes"
   add_foreign_key "posts", "users"
+  add_foreign_key "recipes", "categories"
   add_foreign_key "recipes", "users"
 end
