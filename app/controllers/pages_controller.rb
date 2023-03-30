aff9b7ab0ec486a710d9bbc180a6b100c6f2c3a4
Joel Savage <joel28169@gmail.com>
2023-03-30 16:47:18 +0100

    Added image changer for banner using js, re-positioned home elements

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @rand_recipe = Recipe.all.sample
+    @rand_recipe = Recipe.last
   end
 end
