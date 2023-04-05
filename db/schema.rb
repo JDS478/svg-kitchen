ce381ee77816bcc929d9d7ae9746a0303d52d64c
Joel Savage <joel28169@gmail.com>
2023-04-05 15:03:44 +0100

    Adde users bio and style show to accomodate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_224320) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_135852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_224320) do
     t.string "first_name"
     t.string "last_name"
     t.boolean "admin", default: false
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
