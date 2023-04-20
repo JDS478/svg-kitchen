8c0fd6cd2dbe8c9d9c4bebe28731d177b5ec1072
Joel Savage <joel28169@gmail.com>
2023-04-20 10:49:43 +0100

    Added necessary migrations and updated model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_094340) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_20_094650) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_094340) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
   end
 
   create_table "messages", force: :cascade do |t|
