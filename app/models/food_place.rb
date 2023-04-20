1b089a086e2961a7daa88352faa630cfe39711c1
Joel Savage <joel28169@gmail.com>
2023-04-20 14:23:07 +0100

    Map markers and cards for location styling

--- app/models/food_place.rb
+++ app/models/food_place.rb
@@ -2,6 +2,8 @@ class FoodPlace < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
+  has_one_attached :photo
+
   validates :name, presence: true, uniqueness: true
   validates :address, presence: true
 end
